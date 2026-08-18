056b2586c2bd1455820973f049f0c91460e0d127
RevoltStar <poroshinmaksimigorevich@gmail.com>
2026-08-18 16:43:11 +0300

    Увеличиваем лимит PID для параметра ядра kernel.pid_max

--- fic/src/modules/sysctl/submodules/globalkernelprotection/SYSCTL_process_limits.cpp
+++ fic/src/modules/sysctl/submodules/globalkernelprotection/SYSCTL_process_limits.cpp
@@ -6,7 +6,7 @@ SYSCTL_process_limits::SYSCTL_process_limits()
     this->Sysctl::sysctlParameter = "kernel.pid_max";
     this->Sysctl::sysctlParameterValue = "65536";
     this->policyName = "kernel_pid_max_limit";
-    this->policyTypeValue = std::make_unique<IntPolicyTypeValue>(32768, 1048576, 65536);
+    this->policyTypeValue = std::make_unique<IntPolicyTypeValue>(32768, 4194304, 65536);
 }
 
 bool SYSCTL_process_limits::apply()
