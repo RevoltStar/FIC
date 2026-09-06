6c12d4b7301ffa413cb6dfd6b0430f637368e818
RevoltStar <poroshinmaksimigorevich@gmail.com>
2026-09-06 13:02:11 +0300

    Снижаем поверхность DoS-атак (чтобы непривелигированный пользователь не мог на долго задерживать демон)

--- fic/src/session/SessionEventServer.h
+++ fic/src/session/SessionEventServer.h
@@ -25,7 +25,7 @@ public:
 private:
     static constexpr std::size_t MaxRequestBytes = 4096;
     static constexpr std::size_t MaxPending = 128;
-    static constexpr int ClientPayloadTimeoutMilliseconds = 1000;
+    static constexpr int ClientPayloadTimeoutMilliseconds = 200;
     int serverFd_;
     Validator validator_;
     Reconciler reconciler_;
